800888a0d59748814abc5beec286c1965c1a8cf1
Cameron Patterson <parz3val83@gmail.com>
2023-11-25 08:52:25 -0600

    Add files via upload

---
+++ HW5_271.py
@@ -0,0 +1,49 @@
+import math 
+import planck
+import numpy as np
+
+t_1=planck.radiance(3,1)
+
+
+print("Actual Val" +"\n", t_1)
+
+interp=int(input("enter your interpolattion degree: "))
+interp_1= interp+1
+row = int(input("user input of precission: "))
+
+freq = 3
+dim = ((row,interp_1))
+table= np.ones(dim)
+
+frac= 0.5
+
+cord=0
+
+for i in range(0,row): #row  
+
+    initial=freq-frac*interp
+    
+    for r in range(0,interp_1): # collumn
+                
+        if r== 0:
+                    
+            table[i,r]= initial
+            
+        if r%2==0:
+             r+=1
+        
+                    
+        else:
+            table[i,r]=initial + frac
+            
+                
+        initial+= frac
+        print(table)
+        
+            
+    frac*=1/2
+    i+=1
+    
+
+print("\n" + "Data to precision " + str(interp) +"\n",table)
+
